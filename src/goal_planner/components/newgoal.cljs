ba5852ab3eeaada43a289012840c23659242a271
peterjewicz <peterjewicz@totalwebconnections.com>
2019-06-06 07:41:03 -0500

    add milestone func

--- src/goal_planner/components/newgoal.cljs
+++ src/goal_planner/components/newgoal.cljs
@@ -6,6 +6,7 @@
 
 (defn save-goal [goal]
   "Saves our goal to localStorage"
+  (print (:milestones @goal))
   (print (get-date-by-days 2)))
 
 (defn generate-default-milestones [details value]
@@ -13,25 +14,20 @@
   (swap! details conj {:criteria value})
 
   (let [quarterValue (/ value 4)]
-    (swap! details conj {:milestones [{:date "1/2/18" :value quarterValue}
+    (swap! details conj {:milestones [{:date "1/2/18" :value quarterValue} ; TODO we need to actually generate dates
                  {:date "1/3/18" :value (* 2 quarterValue)}
                  {:date "1/4/18" :value (* 3 quarterValue)}
                  {:date "1/5/18" :value (* 4 quarterValue)}]})))
 
 (defn add-milestone [details]
   "adds a new milestone by adding a blank map to the details atom"
-)
+  (swap! details conj {:milestones (conj (:milestones @details) {:date "" :value ""})}))
 
 (defn remove-milestone [details index]
-  "removes a milestone by removing index from details and rebalancing"
-  (print index)
+  "removes a milestone by removing index from details atom"
   (let [milestones (:milestones @details)]
-  (print (concat (subvec milestones 0 index)
-            (subvec milestones (inc index))))
   (swap! details conj {:milestones (into [](concat (subvec milestones 0 index)
-            (subvec milestones (inc index))))} )
-)
-)
+            (subvec milestones (inc index))))})))
 
 (defn render [state]
   (let [details (atom {:title "" :criteria "" :current 0
@@ -60,16 +56,14 @@
           [:h3.borderText "Milestones"]
           (if (<  0 (count (:criteria @details)))
             (let [milestones (:milestones @details)]
-              (print milestones)
               (doall (map-indexed (fn [index milestone]
-                (print milestone)
                 [:div.milestoneItem {:key (str index "-" (:value milestone))} ;we append the value here so the dispaly updates otherwise the same key renders the same content
                   [:input.milestoneDate {:type "text" :defaultValue (:date milestone)}]
                   [:input.milestoneValue {:type "text" :defaultValue (:value milestone)}]
                   [:p.removeMilestone {:on-click #(remove-milestone details index)} "-"]]
-              ) milestones)))
-            [:button.addMilestone.secondary "+"]
-          )]
+              ) milestones))))
+          (if (<  0 (count (:criteria @details))) ; TODO make this wrapped in the one above
+            [:button.addMilestone.secondary {:on-click #(add-milestone details)} "+"])]
         [:button.primary.saveGoal {:on-click #(save-goal details)} "Save Goal"]])))
 
 
