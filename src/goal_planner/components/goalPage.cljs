a769252cfa1455edc25edc7cfa586222ac2c6e43
peterjewicz <peterjewicz@totalwebconnections.com>
2019-06-07 17:16:01 -0500

    add progress worknig

--- src/goal_planner/components/goalPage.cljs
+++ src/goal_planner/components/goalPage.cljs
@@ -1,16 +1,22 @@
 (ns goal-planner.components.goalPage
   (:require [reagent.core :as reagent :refer [atom]]
-            [goal-planner.state.state :refer [handle-state-change]]))
+            [goal-planner.state.state :refer [handle-state-change]]
+            [goal-planner.scripts.localforageApi :as api]))
 
-
-
+(defn add-progress [goal progress]
+  (api/update-goal (update-in goal [:progress] conj {:value @progress :date "now"}))
+  (reset! progress {:value 0})) ;TODO update input too and cleanup
 
 (defn render [state]
   (fn []
-    (let [goal (:activeGoal @state)]
+    (let [goal (:activeGoal @state)
+          progress (atom 0)]
       [:div.GoalPage.ViewPage.View {:class (:goalpage (:activeView @state))}
         [:div.GoalPage.header
           [:p {:on-click #(handle-state-change "update-current-view" "home")} "<- Back"]
           [:p (:title goal)]]
         [:h1 (:title goal)]
-    ])))
+        [:div.GoalPage.progress
+          [:h3.borderText "Add Progress"]
+          [:input {:type "text" :placeholder "Progress" :on-change #(reset! progress (-> % .-target .-value))}]
+          [:button.primary {:on-click #(add-progress goal progress)} "Save Progres"]]])))
--- src/goal_planner/scripts/localforageApi.cljs
+++ src/goal_planner/scripts/localforageApi.cljs
@@ -6,7 +6,7 @@
   (conj details {:used false :created (js/Date.)}))
 
 (defn get-initial-data []
-  "Queries our initial data - projects"
+  "Queries our initial data - projects - also called whenever a new item is added"
   (.then (.getItem (.-localforage js/window) "goals") (fn [value]
                                                     (handle-state-change "update-goals" (js->clj value :keywordize-keys true)))))
 
@@ -15,7 +15,18 @@
   ; TODO logical checks wither here or in the actual component
     (.then (.getItem (.-localforage js/window) "goals") (fn [value]
       (let [currentStorage (js->clj value :keywordize-keys true)]
-        (print (conj currentStorage goal))
         (.then (.setItem (.-localforage js/window) "goals" (clj->js (conj currentStorage goal)) (fn []
+          (get-initial-data)
           (js/alert "Goal Added"))))))))
+
+(defn update-goal [goal]
+  "updates a goal by finding it then overriding it"
+  (.then (.getItem (.-localforage js/window) "goals") (fn [value]
+    (let [currentStorage (js->clj value :keywordize-keys true)]
+      (loop [i 0] ; Little cleaner than doall/for so we only iterate as needed this probably won't get too big anyways
+        (if (= (:title (nth currentStorage i)) (:title goal))
+          (.then (.setItem (.-localforage js/window) "goals" (clj->js (conj (assoc currentStorage i goal))) (fn []
+            (get-initial-data)
+            (js/alert "Progress Updated!"))))
+          (recur (inc i))))))))
 
